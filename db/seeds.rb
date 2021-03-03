f53b1f0ae611d81b485ce97279bfed680b8d6413
Andrew Medhat <skiresstranger@gmail.com>
2021-03-03 16:56:32 +0200

    Rails MVP done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning the database'
+Restaurant.destroy_all
+
+puts 'Creating 5 movies'
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
